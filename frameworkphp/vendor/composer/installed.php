445c06699dec0ee36b95ebb5085b85e6df155569
Emilie <emilie.chnvt@gmail.com>
2025-02-11 18:04:38 +0100

    Initial commit

---
+++ frameworkphp/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'esdlyonb1-2425/frameworkphp',
+        'pretty_version' => '1.6',
+        'version' => '1.6.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'esdlyonb1-2425/frameworkphp' => array(
+            'pretty_version' => '1.6',
+            'version' => '1.6.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
